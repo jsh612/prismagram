6762369abbbdb02803df55ff450b4d87c41b7037
seungho Jeon <urijsh612@gmail.com>
2019-11-21 22:21:42 +0900

    일부 코드수정

--- src/api/Comment/addComment/addComment.js
+++ src/api/Comment/addComment/addComment.js
@@ -1,9 +1,8 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { isAuthenticated } from "../../../../middlewares";
 
 export default {
   Mutation: {
-    addComment: async (_, args, { request }) => {
+    addComment: async (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request); // 로그인한 유저만 코멘트 작성 가능
       const { text, postId } = args;
       const { user } = request;
