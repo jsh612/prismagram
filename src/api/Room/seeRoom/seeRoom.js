811c2464655a6d560703a0a03c1570935678208e
seungho Jeon <urijsh612@gmail.com>
2019-12-02 19:13:58 +0900

    #3.25 seeRoom Resolver

---
+++ src/api/Room/seeRoom/seeRoom.js
@@ -0,0 +1,23 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { ROOM_FRAGMENT } from "../../../fragments";
+
+export default {
+  Query: {
+    seeRoom: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { roomId } = args;
+      const { user } = request;
+      // 해당 room이 존재해는지 알아보기.
+      const canSee = prisma.$exists.room({
+        participants_some: {
+          id: user.id
+        }
+      });
+      if (canSee) {
+        return prisma.room({ id: roomId }).$fragment(ROOM_FRAGMENT);
+      } else {
+        throw Error("해당 대화방을 볼 수 없습니다.");
+      }
+    }
+  }
+};
